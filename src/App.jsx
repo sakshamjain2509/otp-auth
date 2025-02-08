d2dc725d3c1ec597e84c8531af57e90dbade8f0e
Saksham Jain <saksham.jain.csit.2021@miet.ac.in>
2025-02-08 14:39:57 +0530

    readded

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Auth from './components/auth'
+import Auth from './components/Auth'
 
 function App() {
   const [count, setCount] = useState(0)
